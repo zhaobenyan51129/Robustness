663709b531c252a42b0df209f6db71add49700c2
zhaobenyan <zhaobenyan51129@gmail.com>
2023-03-04 06:42:35 +0000

    3.4

--- config/hello.sh
+++ config/hello.sh
@@ -1,47 +1,53 @@
 #! /bin/bash
 
-# $#获取参数总个数  -ne 不等于
-:<<!  
-[ $# -ne 2 ] && {
-      echo "must be two args"
-      exit 119  #终止程序运行，且返回119状态码，提供给当前shell的$?变量
-}
-echo ok
-echo "当前脚本的id是: $$"  #$$返回当前脚本的id
-!
+# # $#获取参数总个数  -ne 不等于
+# :<<!  
+# [ $# -ne 2 ] && {
+#       echo "must be two args"
+#       exit 119  #终止程序运行，且返回119状态码，提供给当前shell的$?变量
+# }
+# echo ok
+# echo "当前脚本的id是: $$"  #$$返回当前脚本的id
+# !
 
-:<<!
-#函数
-print_usage(){
-      printf "Please enter an integer!!!\n"
-      # 给脚本执行结果赋予一个状态码
-      exit 3
-}
+# :<<!
+# #函数
+# print_usage(){
+#       printf "Please enter an integer!!!\n"
+#       # 给脚本执行结果赋予一个状态码
+#       exit 3
+# }
 
-#read -p "提示信息" 接受用户输入的变量
-read -p "Please input your number" firstnum
-#-n 判断字符串是否为空
-#sed 将firstnum中的数字替换为空，剩下非数字的内容
-if [ -n "`echo $firstnum|sed 's/[0-9]//g'`" ]
-      then  #非空
-            print_usage
-fi
-!
-
-
-:<<!
-#判断文件名后缀是否合法
-if expr "$1" ":" ".*\.jpg" &> /dev/null
-      then
-            echo "这是一个jpg文件"
-else
-      echo "这不是jpg文件"
-fi
-!
-
-# 删除指定文件夹下（Shell文件夹）所有test.txt
-#find Shell -name test.txt |xargs rm -rf
+# #read -p "提示信息" 接受用户输入的变量
+# read -p "Please input your number" firstnum
+# #-n 判断字符串是否为空
+# #sed 将firstnum中的数字替换为空，剩下非数字的内容
+# if [ -n "`echo $firstnum|sed 's/[0-9]//g'`" ]
+#       then  #非空
+#             print_usage
+# fi
+# !
 
 
+# :<<!
+# #判断文件名后缀是否合法
+# if expr "$1" ":" ".*\.jpg" &> /dev/null
+#       then
+#             echo "这是一个jpg文件"
+# else
+#       echo "这不是jpg文件"
+# fi
+# !
+
+# # 删除指定文件夹下（Shell文件夹）所有test.txt
+# #find 文件夹名 -name test.txt |xargs rm -rf
+
+#替换指定目录下的文件名 sed 's/pattern/replace/' 末尾添加g用于替换所有匹配项，而不仅仅替换第一个匹配项。
+cd /home/zhaobenyan/dataset/output/driftgrating_32x32/heatmap
+for file in `ls | grep .png`
+do
+      newfile=`echo $file | sed 's/:/_/g'`
+      mv $file $newfile
+done
 
 
--- repeat/heatmap.py
+++ repeat/heatmap.py
@@ -9,7 +9,7 @@ np.set_printoptions(threshold=np.inf)  #使输出数据完整显示
 import warnings
 warnings.filterwarnings('ignore')
 
-#读取所有time所有contrast重复10次的数据，输出：(5,10,10,3840)
+#读取所有time所有contrast重复10次的数据，输出：fr_vector(5,10,10,3840),error_vector(5,10,10,3840)
 def merge_data_fr():
     fr_list=[]
     for time in times:
@@ -18,7 +18,17 @@ def merge_data_fr():
                 fr_vector=read_output(dir+'/contrast{}/fr_time{}.npz'.format(k+1,time))
                 fr_list.append(fr_vector[i])
     fr_vector = np.array(fr_list).reshape(len(times),n_pic, repeat, -1)
-    return fr_vector 
+    error_list=[]
+    for time in times:
+        fr_tensor_time = fr_vector[time-1] #(10,10,3840)
+        error_pic=[]
+        for i in range(10):#n_pic):
+            fr= fr_tensor_time[i]#(10,3840)
+            error=np.abs(fr-fr[0])#(10,3840)
+            error_pic.append(error)
+        error_list.append(error_pic)
+    error_vector=np.array(error_list)
+    return fr_vector, error_vector
 
 ##读取固定time所有contrast第一次重复的数据，输出：(10,512*nt)
 #时间不同nt不同，因此不能所有时间同时读取
@@ -49,10 +59,11 @@ def plot_fr_error(fr_vector):
     for time in times:
         fr_tensor = fr_vector[time-1] #(10,10,3840)
         fr_mean = np.mean(fr_tensor,axis=1)
-        #图1：都以fr_mean上升排序
-        fig = plt.figure(figsize=(60,40),dpi=200)
+
+        #图1：重复十次，fr的值（按fr_mean上升排序）
+        fig1 = plt.figure(figsize=(60,18),dpi=200)
         for i in range(10):#n_pic):
-            fig.add_subplot(4,5,i+1)
+            fig1.add_subplot(2,5,i+1)
             fr_mean_df=pd.DataFrame(fr_mean[i]).T.round(2)
             fr_df = pd.DataFrame(fr_tensor[i])#(10,3840)
             df1=fr_mean_df.append(fr_df, ignore_index=True)
@@ -66,10 +77,18 @@ def plot_fr_error(fr_vector):
                     )
             plt.title("fr:contrast={}".format((i+1)/20),fontsize = 20)
             ax1.invert_yaxis()  #将y轴顺序颠倒
-            # ax1.set_xticklabels(ax1.get_xticklabels(), rotation=45)  #旋转x轴坐标数据的方向
             ax1.set_yticklabels(ax1.get_yticklabels(), rotation=0)  #旋转y轴坐标数据的方向
+            plt.title("fr:contrast={}".format((i+1)/20),fontsize = 20)
+        plt.suptitle('fr,time={}s'.format(time), ha = 'left',fontsize = 30, weight = 'extra bold')
+        plt.savefig(os.path.join(dir+'/heatmap', 'fr,time={}s'.format(time)))#第一个是指存储路径，第二个是图片名字
+        plt.close()
 
-            fig.add_subplot(4,5,i+11)
+        #图2，重复十次error的值（按fr_mean上升排序）
+        fig2 = plt.figure(figsize=(60,18),dpi=200)
+        for i in range(10):#n_pic):
+            fig2.add_subplot(2,5,i+1)
+            fr_mean_df=pd.DataFrame(fr_mean[i]).T.round(2)
+            fr_df = pd.DataFrame(fr_tensor[i])#(10,3840)
             error_df=(fr_df-fr_df.iloc[0]).abs()
             df2=fr_mean_df.append(error_df, ignore_index=True)
             df2=df2.sort_values(by=0,axis=1,ascending=True)
@@ -81,68 +100,61 @@ def plot_fr_error(fr_vector):
                         vmax=2,
                     )
             ax2.invert_yaxis()  #将y轴顺序颠倒
-            # ax2.set_xticklabels(ax2.get_xticklabels(), rotation=45)  #旋转x轴坐标数据的方向
             ax2.set_yticklabels(ax2.get_yticklabels(), rotation=0)  #旋转y轴坐标数据的方向
             plt.title("error:contrast={}".format((i+1)/20),fontsize = 20)
-        plt.suptitle('sorted by fr,time={}s'.format(time), ha = 'left',fontsize = 30, weight = 'extra bold')
-        plt.savefig(os.path.join(dir, 'sorted by fr,time={}s'.format(time)))#第一个是指存储路径，第二个是图片名字
+        plt.suptitle('error,sorted by fr,time={}s'.format(time), ha = 'left',fontsize = 30, weight = 'extra bold')
+        plt.savefig(os.path.join(dir+'/heatmap', 'error,sorted by fr,time={}s'.format(time)))#第一个是指存储路径，第二个是图片名字
         plt.close()
         
-        #图2：fr以fr_mean上升排序,error以error_mean排序
-        fig = plt.figure(figsize=(60,40),dpi=200)
+        #图3：error,以error_mean排序
+        fig3 = plt.figure(figsize=(60,18),dpi=200)
         for i in range(10):#n_pic):
-            ax = fig.add_subplot(4,5,i+1)
+            fig3.add_subplot(2,5,i+1)
             fr_mean_df=pd.DataFrame(fr_mean[i]).T.round(2)
             fr_df = pd.DataFrame(fr_tensor[i])#(10,3840)
-            df1=fr_mean_df.append(fr_df, ignore_index=True)
-            df1=df1.sort_values(by=0,axis=1,ascending=True)
-            df1.columns = df1.loc[0]
-            df1 = df1[1:]
-            ax1=sns.heatmap(data=df1,
-                        cmap=plt.get_cmap('Greens'),
-                        vmax=60,
-                    )
-            plt.title("fr:contrast={}".format((i+1)/20),fontsize = 20)
-            ax1.invert_yaxis()  #将y轴顺序颠倒
-            # ax1.set_xticklabels(ax1.get_xticklabels(), rotation=45)  #旋转x轴坐标数据的方向
-            ax1.set_yticklabels(ax1.get_yticklabels(), rotation=0)  #旋转y轴坐标数据的方向
-
-            fig.add_subplot(4,5,i+11)
             error_df=(fr_df-fr_df.iloc[0]).abs().round(2)
             error_df.loc[10] = error_df.apply(lambda x: x[1:10].mean(),axis=0).round(2)
             df2=error_df.sort_values(by=10,axis=1,ascending=True)
             df2.columns = df2.loc[10]
             df2 = df2[:10]
+            df2.index = np.arange(1,11)
             ax2=sns.heatmap(data=df2,
                         cmap=plt.get_cmap('Greens'),
                         vmin=0,
                         vmax=2,
                     )
             ax2.invert_yaxis()  #将y轴顺序颠倒
-            # ax2.set_xticklabels(ax2.get_xticklabels(), rotation=45)  #旋转x轴坐标数据的方向
             ax2.set_yticklabels(ax2.get_yticklabels(), rotation=0)  #旋转y轴坐标数据的方向
             plt.title("error:contrast={}".format((i+1)/20),fontsize = 20)
-        plt.suptitle('sorted by mean_error,time={}s'.format(time), ha = 'left',fontsize = 30, weight = 'extra bold')
-        plt.savefig(os.path.join(dir, 'sorted by mean_error,time={}s'.format(time)))#第一个是指存储路径，第二个是图片名字
+        plt.suptitle('error,sorted by mean_error,time={}s'.format(time), ha = 'left',fontsize = 30, weight = 'extra bold')
+        plt.savefig(os.path.join(dir+'/heatmap', 'error,sorted by mean_error,time={}s'.format(time)))#第一个是指存储路径，第二个是图片名字
         plt.close()
 
 #fr_mean和fr_error随time和contrast的变化，fr_vector:(5,10,10,3840) mode:对3840个neuron取二范数还是均值
-def plot_time_contrast(fr_vector,mode):
+def plot_time_contrast(fr_vector,error_vector,mode):
     #先对3480个nuero取平均再对重复次数取平均
     fr_mean=np.mean(fr_vector,axis=3)  #(5,10,10)
     fr_mean=np.mean(fr_mean,axis=2)    #(5,10)
+    error_mean=np.mean(error_vector,axis=3)  #(5,10,10)
+    error_mean=np.mean(error_mean,axis=2)    #(5,10)
     #先对3480个nuero取二范再对重复次数取平均
     fr_norm=np.linalg.norm(fr_vector,axis=3)
     fr_norm=np.mean(fr_norm,axis=2)
+    error_norm=np.linalg.norm(error_vector,axis=3)  #(5,10,10)
+    error_norm=np.mean(error_norm,axis=2) 
 
     if mode =='mean':
-        df = pd.DataFrame(fr_mean)
+        df_fr = pd.DataFrame(fr_mean)
+        df_error=pd.DataFrame(error_mean)
     else:
-        df = pd.DataFrame(fr_norm)
+        df_fr = pd.DataFrame(fr_norm)
+        df_error=pd.DataFrame(error_norm)
     #对fr
     fig = plt.figure(figsize=(10,3),dpi=200)
     fig.add_subplot(1,2,1)
-    ax1=sns.heatmap(data=df,  #数据（dataframe型）（5，10）
+    df_fr.index=[1,2,3,4,5]
+    df_fr.columns=["%.2f"%i for i in list(np.arange(0.05,0.55,0.05))]
+    ax1=sns.heatmap(data=df_fr,  #数据（dataframe型）（5，10）
                     cmap=plt.get_cmap('Greens'),  #颜色风格
                     annot=True,  #是否显示每个方块的数据
                     fmt=".3f",   #每个方块的数据显示三位小数
@@ -156,10 +168,12 @@ def plot_time_contrast(fr_vector,mode):
     ax1.set_ylabel('time/s',fontsize=5 )# 设置y轴标签的字体大小和字体颜色
     cbar1 = ax1.collections[0].colorbar #获取图例
     cbar1.ax.tick_params(labelsize=3) #设置图例的字号
+    plt.title("fr",fontsize = 6)
     #对error
     fig.add_subplot(1,2,2)
-    error_df=(df - df.iloc[0]).abs().round(2)
-    ax2=sns.heatmap(data=error_df,
+    df_error.index=[1,2,3,4,5]
+    df_error.columns=["%.2f"%i for i in list(np.arange(0.05,0.55,0.05))]
+    ax2=sns.heatmap(data=df_error,
                     cmap=plt.get_cmap('Greens'),
                     annot=True,
                     fmt=".3f",
@@ -173,6 +187,7 @@ def plot_time_contrast(fr_vector,mode):
     ax2.set_ylabel('time/s',fontsize=5 )# 设置y轴标签的字体大小和字体颜色
     cbar2 = ax2.collections[0].colorbar #获取图例
     cbar2.ax.tick_params(labelsize=3) #设置图例的字号
+    plt.title("error",fontsize = 6)
     plt.suptitle('time_contrast_{}'.format(mode), ha = 'left',fontsize = 8, weight = 'extra bold')
     plt.savefig(os.path.join(dir+'/heatmap', 'time_contrast_{}'.format(mode)))#第一个是指存储路径，第二个是图片名字
     plt.close()
@@ -210,11 +225,11 @@ if __name__ == "__main__" :
     times=[1,2,3,4,5]  #运行时间
     n_pic=10      #图片个数
     repeat=10    #重复次数
-    dir='/home/zhaobenyan/dataset/output/driftgrating_32x32'    #需要读取的数据所在路径
-    fr_vector=merge_data_fr()
-    plot_fr_error(fr_vector)
-    # plot_time_contrast(fr_vector,mode='norm')
-    # plot_time_contrast(fr_vector,mode='mean')
+    dir='/home/zhaobenyan/dataset/output/grating_32x32'    #需要读取的数据所在路径
+    fr_vector,error_vector=merge_data_fr()
+    # plot_fr_error(fr_vector)
+    plot_time_contrast(fr_vector,error_vector,mode='norm')
+    plot_time_contrast(fr_vector,error_vector,mode='mean')
     # compute_ratio_list() #运行一次即可
     # plot_lgn_ratio()
     
